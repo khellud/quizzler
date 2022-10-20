8ada30410a869a6d7391ae0c7f27cf5f498cee97
khellud <khalidhamid993@gmail.com>
2022-10-20 12:17:51 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: QuizePage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizePage extends StatefulWidget {
+  const QuizePage({super.key});
+
+  @override
+  State<QuizePage> createState() => _QuizePageState();
+}
+
+class _QuizePageState extends State<QuizePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.green,
+            child: TextButton(
+              onPressed: () {},
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
